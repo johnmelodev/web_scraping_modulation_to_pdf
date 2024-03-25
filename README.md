03c475c448568166c75e590757ed8a0767d07dc7
johnmelodev <joaohenrique1231500@gmail.com>
2024-03-25 09:23:25 -0300

    House search modification

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Web_Scraping_companies_data
+
+Demanda de uma empresa de imoveis em que o time envia uma lista para ser impressa pelo outro time para organizar os imoveis de interesse de compra na cidade
+
+empresa de imoveis
+
+pip3 install -r requirements.txt
--- app.py
+++ app.py
@@ -24,15 +24,15 @@ print('Response Body: ', response.content)
 '''
 
 # File name
-workbook = openpyxl.load_workbook('cnpjs.xlsx')
+workbook = openpyxl.load_workbook('house_list.xlsx')
 # Page name
 sheet_data = workbook['data']
 # Column
-cnpjs = []
+house_list = []
 # Row (iterate over the column = min_col1 to max_col1 and over the row min_row2 to max_row11)
 for line in sheet_data.iter_rows(min_col=1, max_col=1, min_row=2, max_row=11):
     for cell in line:
-        cnpjs.append(cell.value)
+        house_list.append(cell.value)
 
 
 def start_driver():
@@ -44,11 +44,15 @@ def start_driver():
     for argument in arguments:
         chrome_options.add_argument(argument)
 
-    chrome_options.add_experimental_option('prefs', {
-        'download.prompt_for_download': False,
-        'profile.default_content_setting_values.notifications': 2,
-        'profile.default_content_setting_values.automatic_downloads': 1
-    })
+    prefs = {
+        'profile.default_content_setting_values': {
+            'cookies': 2,
+            'notifications': 2,
+            'automatic_downloads': 1
+        }
+    }
+
+    chrome_options.add_experimental_option('prefs', prefs)
 
     driver = webdriver.Chrome(service=ChromeService(
         ChromeDriverManager().install()), options=chrome_options)
@@ -58,14 +62,15 @@ def start_driver():
 
 driver = start_driver()
 
-for cnpj in cnpjs:
-    driver.get(f'https://cadastroempresa.com.br/cnpj/{cnpj}')
+for house_list in house_list:
+    driver.get(f'https://www.century21global.com/en/p/{house_list}')
     sleep(3)
     base64_string = driver.print_page()
     pdf_in_bytes = base64.b64decode(base64_string)
 
     # Formatting CNPJ
-    format_cnpj = cnpj.replace('.', ',').replace('-', '').replace('/', '')
+    format_house_list = house_list.replace(
+        '.', ',').replace('-', '').replace('/', '')
 
-    with open(f'./company data/{format_cnpj}.pdf', 'wb') as file:
+    with open(f'./company data/{format_house_list}.pdf', 'wb') as file:
         file.write(pdf_in_bytes)
